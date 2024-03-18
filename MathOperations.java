2a47ef3fbe6395aac4e27ada2fe68f5fa8b77184
vijay920 <109593660+vijay920@users.noreply.github.com>
2024-03-18 13:02:51 +0530

    Create MathOperations.java

---
+++ MathOperations.java
@@ -0,0 +1,24 @@
+public class MathOperations{
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public double add(double a, double b, double c){
+        return a + b + c;
+    }
+    
+    public String add(String a, String b){
+        return a + b;
+    }
+
+    public static void main(String[] args){
+        MathOperations op = new MathOperations();
+        int sumInt = op.add(40, 30);
+        System.out.println("Sum of two integers: " + sumInt);
+        double sumDouble = op.add(17.9, 40.12, 17.31);
+        System.out.println("Sum of three doubles: " + sumDouble);
+        String concatenatedString = op.add("Hello, ", "How are you?!");
+        System.out.println("Concatenation of two strings: " + concatenatedString);
+    }
+}
